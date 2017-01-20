aea3460b72eccf40df142be7cd2f15153089f2d4
popomore <sakura9515@gmail.com>
2017-01-20 13:41:07 +0800

    feat: remove worker client, use app.cluster
    Closes https://github.com/eggjs/egg/issues/278

--- app/extend/application.js
+++ app/extend/application.js
@@ -5,12 +5,10 @@ const assert = require('assert');
 const cluster = require('cluster-client');
 const Service = require('../../lib/core/base_service');
 const view = require('../../lib/core/view');
-const AppWorkerClient = require('../../lib/core/app_worker_client');
 const util = require('../../lib/core/util');
 const Singleton = require('../../lib/core/singleton');
 
 const KEYS = Symbol('Application#keys');
-const APP_CLIENTS = Symbol('Application#appClients');
 const VIEW = Symbol('Application#View');
 const LOCALS = Symbol('Application#locals');
 const LOCALS_LIST = Symbol('Application#localsList');
@@ -57,45 +55,6 @@ module.exports = {
    * @since 1.0.0
    */
   Service,
-
-  /**
-   * AppWorkerClient class
-   * @member {AppWorkerClient} Application#AppWorkerClient
-   */
-  AppWorkerClient,
-
-  /**
-   * 当前进程实例化的 AppWorkerClient 集合
-   * @member {Map} Application#appWorkerClients
-   * @private
-   */
-  get appWorkerClients() {
-    if (!this[APP_CLIENTS]) {
-      this[APP_CLIENTS] = new Map();
-    }
-    return this[APP_CLIENTS];
-  },
-
-  /**
-   * 创建一个 App worker "假"客户端
-   * @method Application#createAppWorkerClient
-   * @param {String} name 客户端的唯一名字
-   * @param {Object} impl - 客户端需要实现的 API
-   * @param {Object} options
-   *   - {Number|String} responseTimeout - 响应超时间隔，默认为 3s
-   * @return {AppWorkerClient} - client
-   */
-  createAppWorkerClient(name, impl, options) {
-    options = options || {};
-    options.name = name;
-    options.app = this;
-
-    const client = new AppWorkerClient(options);
-    Object.assign(client, impl);
-    // 直接 ready
-    client.ready(true);
-    return client;
-  },
 
   /**
    * 创建一个单例并添加到 app/agent 上
